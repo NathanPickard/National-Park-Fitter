c67c1f8215383de3c105c7c6b179a019bde84e19
Nathan Pickard <nathan.pickard87@gmail.com>
2019-03-07 17:21:29 -0800

    Adding routing module to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
