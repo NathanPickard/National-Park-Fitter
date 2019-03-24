45ba54010018c0fc3c0df3560a074874cbde13ed
Nathan Pickard <nathan.pickard87@gmail.com>
2019-03-24 12:54:27 -0700

    Implementing router-outlet for stepper and about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
+import { StepperComponent } from './stepper/stepper.component';
 import { AboutComponent } from './about/about.component';
 
 const appRoutes: Routes = [
+  {
+    path: '', component: StepperComponent
+  },
   {
     path: 'about', component: AboutComponent
   }
