c77b3c8207ef9d2f83d40c2d494448214b483dbc
Nathan Pickard <nathan.pickard87@gmail.com>
2019-04-20 23:10:09 -0700

    States added to selector

--- src/app/park/park.component.ts
+++ src/app/park/park.component.ts
@@ -20,6 +20,16 @@ export class ParkComponent implements OnInit {
   stepperSubmitted: boolean = false;
   resultsFound: boolean = false;
 
+  states: string[] = [
+    'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware',
+    'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky',
+    'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
+    'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico',
+    'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
+    'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+  ];
+
   constructor(private _formBuilder: FormBuilder,
     private searchService: SearchService) { }
 
