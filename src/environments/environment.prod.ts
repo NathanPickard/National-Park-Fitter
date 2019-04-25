ed7fae31743f385538a6d934cfb96809f9c747fe
Nathan Pickard <nathan.pickard87@gmail.com>
2019-04-24 21:09:38 -0700

    Layout adjustment for park and search park results

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   NPS_API_KEY: 'uYQbQmGs618vM9mHWlgajs0UHlmXmKWup3WN40GT',
-  NPS_BASE_URL: 'https://api.nps.gov/api/v1/'
+  NPS_BASE_URL: 'https://api.nps.gov/api/v1/',
+  RIDB_API_KEY: '9038f481-6e5d-476a-b358-3c6552347cc7',
+  BIDB_BASE_URL: ''
 };
