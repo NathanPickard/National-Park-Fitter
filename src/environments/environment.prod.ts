1fb9aa89c8a5e0666651f6dbc294d5d5f4683159
Nathan Pickard <nathan.pickard87@gmail.com>
2019-07-21 22:28:46 -0700

    es2015 target

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
   RIDB_API_KEY: '9038f481-6e5d-476a-b358-3c6552347cc7',
   BIDB_BASE_URL: '',
   DARK_SKY_API_KEY: 'c084262a63603f46bcc265a0c5acb6e1',
-  DARK_SKY_BASE_UR: 'https://api.darksky.net/forecast/'
+  DARK_SKY_BASE_URL: 'https://api.darksky.net/forecast/'
 };
