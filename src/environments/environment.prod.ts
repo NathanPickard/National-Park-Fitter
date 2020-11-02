96ab855c99d19e85d261b8dfdcd7d5958115040b
Nathan Pickard <nathan.pickard87@gmail.com>
2020-11-01 21:57:06 -0800

    Environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,8 @@ export const environment = {
   NPS_BASE_URL: 'https://api.nps.gov/api/v1/',
   RIDB_API_KEY: '9038f481-6e5d-476a-b358-3c6552347cc7',
   RIDB_BASE_URL: '',
-  OPEN_WEATHER_API_KEY: 'cca81332aa416bea35d977c1e43d68f7',
-  OPEN_WEATHER_BASE_URL: 'https://api.openweather.org/data/2.5/'
+  // OPEN_WEATHER_API_KEY: 'cca81332aa416bea35d977c1e43d68f7',
+  // OPEN_WEATHER_BASE_URL: 'https://api.openweather.org/data/2.5/',
+  WEATHER_API_KEY: '3a4569f9956ea7bd05433eabcc5881dd',
+  WEATHER_BASE_URL: 'http://api.weatherstack.com/'
 };
