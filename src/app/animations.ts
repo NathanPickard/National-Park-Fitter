c691270a2427b26cb4b9af8034f74449dd1d4da1
Nathan Pickard <nathan.pickard87@gmail.com>
2021-07-30 14:39:03 -0700

    Added fade transition to routing

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -2,18 +2,53 @@ import { trigger, animate, transition, style, group, query } from '@angular/anim
 
 export const routerTransition = trigger('routerTransition', [
 
+  // Slide transition:
+  // transition('* => *', [
+  //   query(':enter, :leave', style({ position: 'fixed', width: '100%' })
+  //     , { optional: true }),
+  //   group([
+  //     query(':enter', [
+  //       style({ transform: 'translateX(-100%)' }),
+  //       animate('0.4s ease-in-out', style({ transform: 'translateX(0%)' }))
+  //     ], { optional: true }),
+  //     query(':leave', [
+  //       style({ transform: 'translateX(0%)' }),
+  //       animate('0.4s ease-in-out', style({ transform: 'translateX(100%)' }))
+  //     ], { optional: true }),
+  //   ])
+  // ])
+
+
+  // Fade transition:
   transition('* => *', [
-    query(':enter, :leave', style({ position: 'fixed', width: '100%' })
-      , { optional: true }),
+    query(':enter, :leave',
+      style({ position: 'fixed', width: '100%' }),
+      { optional: true }),
+
     group([
-      query(':enter', [
-        style({ transform: 'translateX(-100%)' }),
-        animate('0.4s ease-in-out', style({ transform: 'translateX(0%)' }))
-      ], { optional: true }),
-      query(':leave', [
-        style({ transform: 'translateX(0%)' }),
-        animate('0.4s ease-in-out', style({ transform: 'translateX(100%)' }))
-      ], { optional: true }),
+      query(':enter',
+        [
+          style({ opacity: 0 })
+        ],
+        { optional: true }
+      ),
+      query(':leave',
+        [
+          style({ opacity: 1 }),
+          animate('0.2s',
+            style({ opacity: 0 }))
+        ],
+        { optional: true }
+      ),
+
+      query(':enter',
+        [
+          style({ opacity: 0 }),
+          animate('0.2s',
+            style({ opacity: 1 }))
+        ],
+        { optional: true }
+      )
     ])
   ])
 ]);
